2e1e33961fffdb0de4aae1f2a5469fc47599933e
Edwin Jain <Edwin.Jain@tufts.edu>
2018-05-17 01:52:48 -0400

    Added documentation to modules

--- rr_model/init_graph.py
+++ rr_model/init_graph.py
@@ -1,6 +1,11 @@
+"""
+	This module initializes graph topologies for the RegimeModel, using
+	the create_graph function.
+"""
+
 import networkx as nx
 
-
+# One function per possible network topology
 def random_graph(n, p): 
     return nx.erdos_renyi_graph(n=n, p=p)
 
@@ -11,6 +16,7 @@ def scale_free(n, p):
         return nx.barabasi_albert_graph(n=n, m=0.5*p*n + 1)
 
 
+# Initialize shapes to map strings indicating shape to a graph-generator
 shapes = dict.fromkeys(['random', 'erdos_renyi', 'erdos', 'er'], random_graph)
 shapes.update(dict.fromkeys(['small_world', 'watts_strogatz', 'watts', 'ws'], 
                             small_world))
@@ -18,5 +24,12 @@ shapes.update(dict.fromkeys(['scale_free', 'barabasi_albert', 'barabasi', 'ba'],
                             scale_free))
 
 
-def create_graph(shape, num_nodes, p):    
+def create_graph(shape, num_nodes, p):
+	""" Returns a Networkx graph.
+
+	Arguments:
+	shape -- String indicating the network topology
+	num_nodes -- Integer indicating number of nodes
+	p -- Float controlling network connectivity, 0 < p <= 1
+	"""    
     return shapes[shape](num_nodes, p)
--- rr_model/reporters.py
+++ rr_model/reporters.py
@@ -1,10 +1,21 @@
+"""
+    This module contains the reporters of the RegimeModel.
+
+    Functions:
+
+    - Inequality: gini_capacity, gini_resources
+    - Topology: average_clustering, assortativity, number_of_components
+    - Satisfaction: num_satisfied, num_dissatisfied
+
+    All functions take in only a model parameter and return a number.
+"""
+
 import math
 import itertools
 
 import networkx as nx
 
-
-
+# Calculates the gini coefficient for any attribute of agents in the regime
 def gini_coefficient (model, attribute):
     agent_attribute_list = [getattr(a, attribute) for a in model.grid.get_all_cell_contents()]
     total = sum(agent_attribute_list)
@@ -16,16 +27,23 @@ def gini_coefficient (model, attribute):
     except ZeroDivisionError:
         return 0
 
+
+# Inequality Functions: 
 def gini_capacity(model):
     return gini_coefficient(model, 'capacity')
 
 def gini_resources(model):
     return gini_coefficient(model, 'resources')
 
+
+# This function returns the value of a measure on a model's network.
+# It is called by all topology functions.
 def network_measure(model, measure):
     m = getattr(nx, measure)
     return m(model.G)
 
+
+# Topology Functions:
 def average_clustering(model):
     return network_measure(model, "average_clustering")
 
@@ -38,6 +56,8 @@ def assortativity(model):
 def number_of_components(model):
     return network_measure(model, "number_connected_components")
 
+
+# Satisfaction Functions:
 def num_satisfied(model):
     return sum([1 for a in model.grid.get_all_cell_contents() if a.satisfied is True])
 
